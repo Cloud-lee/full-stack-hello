176bbc1cf89c79e1cffd8ab55c289775eb5f0a1c
uno <st9007a@gmail.com>
2017-11-05 23:44:25 +0800

    Use label for call and j-type instructions

--- tests/fib-recursive.s
+++ tests/fib-recursive.s
@@ -1,33 +1,33 @@
 ; init result at #3
-or $0 $0 #3
+     or $0 $0 #3
 
 ; f0 = 0, f1 = 1
-or $0 $0 #1
-or $1 $0 #2
+     or $0 $0 #1
+     or $1 $0 #2
 
-call #6
+     call :func
 
 ; print result
-print #3
-halt
+     print #3
+     halt
 
 ; recursion
-xor #0 #2 #5
-jz #5 #9
-jnz #5 #11
-add #3 #2 #3
-ret
+func xor #0 #2 #5
+     jz #5 :test1
+     jnz #5 :test2
+test1 add #3 #2 #3
+      ret
 
-xor #0 #1 #5
-jz #5 #14
-jnz #5 #15
-ret
+test2 xor #0 #1 #5
+      jz #5 :test3
+      jnz #5 :test4
+test3 ret
 
-sub #0 $1 #0
-call #6
+test4 sub #0 $1 #0
+      call :func
 
-sub #0 $1 #0
-call #6
+      sub #0 $1 #0
+call :func
 
-add #0 $2 #0
-ret
+      add #0 $2 #0
+      ret
--- tests/fib-tailrecursion.s
+++ tests/fib-tailrecursion.s
@@ -1,32 +1,32 @@
 ; init
-or $0 $0 #1
-or $0 $1 #2
+     or $0 $0 #1
+     or $0 $1 #2
 
-or $0 $0 #3
+     or $0 $0 #3
 
 ; call func
-call #6
-print #3
-halt
+     call :tail
+     print #3
+     halt
 
 ; tail recursion
-xor #0 $0 #4
-jz #4 #9
-jnz #4 #11
-or #1 $0 #3
-ret
+tail xor #0 $0 #4
+     jz #4 :iszero
+     jnz #4 :notzero
+iszero or #1 $0 #3
+       ret
 
-xor #0 $1 #4
-jz #4 #14
-jnz #4 #16
-or #2 $0 #3
-ret
+notzero xor #0 $1 #4
+        jz #4 :isone
+        jnz #4 :notone
+isone or #2 $0 #3
+      ret
 
 ; #1 = #2, #2 = #1 + #2
-or #1 $0 #5
-or #2 $0 #1
-add #2 #5 #2
-sub #0 $1 #0
-call #6
-ret
+notone or #1 $0 #5
+       or #2 $0 #1
+       add #2 #5 #2
+       sub #0 $1 #0
+       call :tail
+       ret
 
