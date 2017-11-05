0139f86a36c85242b962670b375a8b69666bc129
uno <st9007a@gmail.com>
2017-11-05 15:42:26 +0800

    Increase coverage of test case of label feature

--- tests/label.s
+++ tests/label.s
@@ -1,6 +1,57 @@
-call :test
+call :testcall
 print "fail"
 halt
 
-test print "success"
+testcall print "call success"
+jmp :testjmp
+print "fail"
 halt
+
+testjmp print "jmp success"
+or $0 $0 #0
+jz #0 :testjz
+print "fail"
+halt
+
+testjz print "jz success"
+add $1 #0 #0
+jnz #0 :testjnz
+print "fail"
+halt
+
+testjnz print "jnz success"
+jge #0 :testjge1
+print "fail"
+halt
+
+testjge1 print "jge success"
+sub #0 $1 #0
+jge #0 :testjge2
+print "fail"
+halt
+
+testjge2 print "jge success"
+jle #0 :testjle1
+print "fail"
+halt
+
+testjle1 print "jle success"
+sub #0 $1 #0
+jle #0 :testjle2
+print "fail"
+halt
+
+testjle2 print "jle success"
+jlt #0 :testjlt
+print "fail"
+halt
+
+testjlt print "jlt success"
+add #0 $2 #0
+jgt #0 :testjgt
+print "fail"
+halt
+
+testjgt print "jgt success"
+halt
+
